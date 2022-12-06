0ed4a5b26034213318b03f04f52f6109298752af
Sontos Sharma <sontossharma926@gmail.com>
2022-12-07 01:53:06 +0600

    about page complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,35 @@ $(document).ready(function(){
     $('.sidebar').removeClass('sactive');
   });
 })
+$('.slider-pic').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: false,
+  centerMode: false,
+  arrows: false,
+  fade: true,
+  autoplay:true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 0,
+  centerMode: false,
+  asNavFor: '.slider-pic',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});
+	
